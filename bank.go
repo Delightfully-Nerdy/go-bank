dd0d64d2c0b2bd4930d47dfd99e596c850759459
Dalitso Kasonde <dkasonde11@gmail.com>
2025-01-12 23:35:14 +0200

    nested if

--- bank.go
+++ bank.go
@@ -25,12 +25,28 @@ func main() {
 		fmt.Print("Your deposit:")
 		var depositAmount float64
 		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+
 		accountBalance += depositAmount
 		fmt.Println("Balance updated! New amount: ", accountBalance)
 	} else if choice == 3 {
 		fmt.Print("Your withdraw:")
 		var withdrawAmount float64
 		fmt.Scan(&withdrawAmount)
+
+		if withdrawAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+		}
+
+		if withdrawAmount > accountBalance {
+			fmt.Println("Invalid amount. You can not withdraw more than you have.")
+			return
+		}
+
 		accountBalance -= withdrawAmount
 		fmt.Println("Money withdrawn! New amount: ", accountBalance)
 	} else {
